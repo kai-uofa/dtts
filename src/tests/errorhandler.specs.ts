5c5d83b375359ff7e6027f3371093cb0db796223
Kai Nguyen <kien@makersempire.com>
2022-06-06 21:32:38 +0930

    Rename testcase for 404

--- src/tests/errorhandler.specs.ts
+++ src/tests/errorhandler.specs.ts
@@ -4,8 +4,8 @@ import app from '../app';
 
 const { NOT_FOUND } = StatusCodes;
 
-describe("GET /redirect - Test a simple api endpoint", () => {
-  it("Hello API Request", async () => {
+describe("GET /redirect - Test request not found", () => {
+  it("404 API Request", async () => {
     const result = await request(app).get("/redirect");
     expect(result.text).toEqual("Not Found. Redirecting to /");
     expect(result.statusCode).toEqual(NOT_FOUND);
