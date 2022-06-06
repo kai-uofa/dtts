e08ee0cd13f2bb2753b8527da77ae3c9ce372df4
Kai Nguyen <kien@makersempire.com>
2022-06-06 15:09:51 +0930

    Minor update for Jest unit test

--- src/tests/index.specs.ts
+++ src/tests/index.specs.ts
@@ -1,12 +1,15 @@
+import StatusCodes from 'http-status-codes';
 import request from 'supertest';
 import app from '../app';
 
 import expected from './index.specs.json';
 
+const { OK } = StatusCodes;
+
 describe("GET / - Test a simple api endpoint", () => {
   it("Hello API Request", async () => {
     const result = await request(app).get("/");
     expect(JSON.parse(result.text)).toEqual(expected);
-    expect(result.statusCode).toEqual(200);
+    expect(result.statusCode).toEqual(OK);
   });
 });
