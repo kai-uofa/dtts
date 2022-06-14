6c182e0b98f8f2a15d200c9d02c1f55933cf46f6
Kai Nguyen <kien@makersempire.com>
2022-06-14 21:29:47 +0930

    Update index test case

--- tests/index.specs.ts
+++ tests/index.specs.ts
@@ -7,17 +7,17 @@ const expected: { [key: string]: any } = {
     title: "Welcome to DTTS api",
     description: "A simple DateTime API using TypeScript",
     days_endpoint: "`.../days` returns the number of days between 2 given dates",
-    wdays_endpoint: "`.../wdays` which returns the number of weekdays between 2 given dates",
+    weekdays_endpoint: "`.../weekdays` which returns the number of weekdays between 2 given dates",
     weeks_endpoint: "`.../weeks` which returns the number of complete weeks between 2 given dates",
     accepted_params: {
         startDate: "Madatory, in standard datetime format (ISO 8601)",
         endDate: "Optional, in standard datetime format (ISO 8601)",
-        convertUnit: "Optional, accept only 'seconds', 'minutes', 'hours', or 'years'",
+        convertUnit: "Optional, accept only 'seconds', 'minutes', 'hours', or 'years'"
     }
 }
 
 describe("GET / - Test a simple api endpoint", () => {
-    it("Hello API Request", async () => {
+    it("Index API Request", async () => {
         const result = await request(app).get("/");
         expect(JSON.parse(result.text)).toEqual(expected);
         expect(result.statusCode).toEqual(OK);
