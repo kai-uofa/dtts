86c0c498c3d0bab592300adefa92baf0e29b7570
Kai Nguyen <kien@makersempire.com>
2022-06-14 20:15:44 +0930

    Fix broken redirect due to 404 status

--- src/middleware/errorhandler.ts
+++ src/middleware/errorhandler.ts
@@ -1,11 +1,11 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
 
-const { NOT_FOUND } = StatusCodes
+const { PERMANENT_REDIRECT } = StatusCodes
 
 /* Redirect to Index for 404 requests */
 function redirectToIndex(req: Request, res: Response) : any {
-    res.redirect(NOT_FOUND, '/');
+    res.redirect(PERMANENT_REDIRECT, '/');
     return;
 }
 
