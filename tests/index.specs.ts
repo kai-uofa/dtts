02166782406b31c63bceedaf90257627004f3c84
Kai Nguyen <kien@makersempire.com>
2022-06-14 22:17:09 +0930

    Fixed typo in index test case

--- tests/index.specs.ts
+++ tests/index.specs.ts
@@ -10,8 +10,8 @@ const expected: { [key: string]: any } = {
     weekdays_endpoint: "`.../weekdays` which returns the number of weekdays between 2 given dates",
     weeks_endpoint: "`.../weeks` which returns the number of complete weeks between 2 given dates",
     accepted_params: {
-        startDate: "Madatory, in standard datetime format (ISO 8601)",
-        endDate: "Optional, in standard datetime format (ISO 8601)",
+        startDate: "Mandatory, in standard date-time format (ISO 8601)",
+        endDate: "Optional, in standard date-time format (ISO 8601)",
         convertUnit: "Optional, accept only 'seconds', 'minutes', 'hours', or 'years'"
     }
 }
